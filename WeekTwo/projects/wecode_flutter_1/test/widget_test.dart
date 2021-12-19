49c843b8d64d1d44a63e3694bee9a525b7decc43
Hooshyar <hooseyr@gmail.com>
2021-12-19 15:00:49 +0300

    Some Apple Configuration added

--- WeekTwo/projects/wecode_flutter_1/test/widget_test.dart
+++ WeekTwo/projects/wecode_flutter_1/test/widget_test.dart
@@ -8,13 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wecode/main.dart';
 import 'package:wecode/src/wecode_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const WeCodeApp());
+    await tester.pumpWidget(WeCodeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
